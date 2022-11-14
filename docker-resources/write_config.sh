32e13959b001691ad87b8035f0a533ab54677342
Natanael Arndt <natanael.arndt@eccenca.com>
2022-11-14 14:29:42 +0100

    Add Dockerfile and relavant resources

---
+++ docker-resources/write_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+JOD_CONFIG_BASEURL="/${JOD_URL#http*\://*/}"
+JOD_CONFIG_URL="${JOD_URL%$JOD_CONFIG_BASEURL}"
+
+export JOD_CONFIG_BASEURL JOD_CONFIG_URL
+
+{ echo "---"; jq -n -r 'env|to_entries[]|(.key+": "+.value)'; echo "---"; } | mustache - /docker-resources/_config.yml.mustache > /tmp/_config.yml
+
+jekyll build --disable-disk-cache --config /tmp/_config.yml
